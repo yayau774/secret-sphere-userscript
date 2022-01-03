8421eafa2884da06ea0f027d83afd78774df35db
yayau774 <neko@neko.jp>
2022-01-03 18:36:33 +0900

    author

--- yy-navibar.user.js
+++ yy-navibar.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       yayau
 // @match        http://www.sssloxia.jp/result/*/c/*.html
 // @icon         https://www.google.com/s2/favicons?domain=sssloxia.jp
 // @grant        none
