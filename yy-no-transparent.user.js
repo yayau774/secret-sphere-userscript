a57f38871cd2b5e9f213ad6b2ae1d04c19c308dd
yayau774 <neko@neko.jp>
2022-01-16 19:43:01 +0900

    update no transparent/ exclude rp2.aspx

--- yy-no-transparent.user.js
+++ yy-no-transparent.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         テーブルの背景透過を避ける
 // @namespace    https://github.com/yayau774/secret-sphere-userscript/
-// @version      0.3
+// @version      0.4
 // @description  
 // @author       yayau
 // @updateURL    https://github.com/yayau774/secret-sphere-userscript/raw/main/yy-no-transparent.user.js
 // @match        http://www.sssloxia.jp/*
+// @exclude      http://www.sssloxia.jp/d/rp2*
 // @icon         https://www.google.com/s2/favicons?domain=sssloxia.jp
 // @grant        none
 // ==/UserScript==
@@ -15,8 +16,8 @@
     // css取得
     const rules = document.styleSheets[0].cssRules;
 
-    //  rgba(r, g, b, alpha)　のうち、rgbを取得するための正規表現
-    const re_rgba = /rgba\((\d+), (\d+), (\d+), \d+\.\d+\)/;
+    //  rgba(r, g, b, alpha)からrgbaを取得するための正規表現
+    const re_rgba = /rgba\((\d+), (\d+), (\d+), (\d+\.\d+)\)/;
 
     //  ルールを見ていく
     for(let rule of rules){
@@ -24,7 +25,11 @@
         if(rule?.style?.getPropertyValue("background-color")){
             let bgc = rule.style.getPropertyValue("background-color");
             console.log("rgba to rgb: " + rule.selectorText);
-            rule.style.setProperty("background-color", bgc.replace(re_rgba, (match, p1, p2, p3, offset, string) => `rgb(${p1}, ${p2}, ${3})`));
+            rule.style.setProperty("background-color", bgc.replace(re_rgba, (match, p1, p2, p3, p4, offset, string) => {
+                //console.log("rgba to rgb: " + rule.selectorText);
+                //console.log(`rgba(${p1}, ${p2}, ${p3}, ${p4}) to rgb(${p1}, ${p2}, ${p3})`);
+                return `rgb(${p1}, ${p2}, ${p3})`;
+            }));
         }
     }
 })();
